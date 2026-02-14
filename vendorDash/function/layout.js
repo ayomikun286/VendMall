0544cfdb7ada7df7bbdda3fdf11bef962c6196d7
AYOMIKUN <edegbaiayomikun@gmail.com>
2026-02-14 15:06:23 +0100

    update

--- vendorDash/function/layout.js
+++ vendorDash/function/layout.js
@@ -5,14 +5,36 @@
 export function profile(){
     const profileDrop = document.getElementById('profileDrop');
     const profileSwich = document.getElementById('profileSwich');
-   
+    const profileArrow = document.getElementById('profileArrow');
     profileSwich.addEventListener('click', (e)=>{
        e.stopPropagation();
             profileDrop.classList.add('show');
+            profileArrow.classList.add('fa-angle-up')
+            profileArrow.classList.remove('fa-angle-down')
     });
     document.addEventListener('click', () => {
          profileDrop.classList.remove('show');
+          profileArrow.classList.add('fa-angle-down')
+           profileArrow.classList.remove('fa-angle-up')
     });
+
+
+    const StorePop = document.getElementById('StorePop');
+    const popUp = document.getElementById('popUp');
+    const storeArrow = document.getElementById('storeArrow')
+    StorePop.addEventListener('click', (e)=>{
+         e.stopPropagation();
+         popUp.classList.add('show');
+         storeArrow.classList.add('fa-angle-up');
+          storeArrow.classList.remove('fa-angle-down')
+    });
+     
+    document.addEventListener('click', ()=>{
+        popUp.classList.remove('show');
+         storeArrow.classList.remove('fa-angle-up');
+          storeArrow.classList.add('fa-angle-down')
+    });
+
 }
 
 
@@ -20,6 +42,7 @@ export function profile(){
 
 export function nameDisplay() {
     const email = document.querySelectorAll('#email');
+    const storeName = document.querySelectorAll('#storeName');
     if (!email) {
         return;
     }
@@ -27,7 +50,13 @@ export function nameDisplay() {
     console.log(currentVendor);
     email.forEach(E => {
         E.textContent = currentVendor.email
-    })
+    });
+   storeName.forEach(SN => {
+     SN.textContent = currentVendor.storeName
+   });
+
+
+
 }
 
 
